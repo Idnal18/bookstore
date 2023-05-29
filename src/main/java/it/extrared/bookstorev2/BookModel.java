0760a28127d2ac00bdac79c2599dcc8b12ed0a75
Idnal18 <98057842+Idnal18@users.noreply.github.com>
2023-05-29 15:45:24 +0200

    Versione funzionante della libreria
    Progetto risviluppato da zero.
    inserisci i dati del libro da inserire e premi Submit per stamparli nella pagina successiva

---
+++ src/main/java/it/extrared/bookstorev2/BookModel.java
@@ -0,0 +1,24 @@
+package it.extrared.bookstorev2;
+
+public class BookModel {
+
+    private String nomeLibro;
+    private String autoreLibro;
+
+    public String getNomeLibro() {
+        return nomeLibro;
+    }
+
+    public void setNomeLibro(String nomeLibro) {
+        this.nomeLibro = nomeLibro;
+    }
+
+    public String getAutoreLibro() {
+        return autoreLibro;
+    }
+
+    public void setAutoreLibro(String autoreLibro) {
+        this.autoreLibro = autoreLibro;
+    }
+    
+}
